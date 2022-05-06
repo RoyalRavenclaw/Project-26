12961e86265bfb3fb78f1c2ed59ef7fe77fc12c9
RoyalRavenclaw <99022593+RoyalRavenclaw@users.noreply.github.com>
2022-05-06 18:41:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ const Constraint = Matter.Constraint;
 var bob1,bob2,bob3, bob4,bob5, roofObject
 var rope1,rope2,rope3, rope4,rope5;
 var world, NImg, EImg, WImg, TImg, OImg, SImg, CImg, RImg, AImg, DImg, LImg;
-var N, E, W, T, O, Ns, S, C, R, A, D, L, Es;
+var side, side1, side2;
 
 function preload(){
 	NImg = loadImage("./assets/N.png");
@@ -33,13 +33,13 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	roofObject=new roof(400,200,300,20);
+	roofObject=new roof(400,300,500,20);
 
-	bob1 = new bob(320,450,40)
-	bob2 = new bob(360,450,40)
-	bob3 = new bob(400,450,40)
-	bob4 = new bob(440,450,40)
-	bob5 = new bob(480,450,40)
+	bob1 = new bob(320,500,40)
+	bob2 = new bob(360,500,40)
+	bob3 = new bob(400,500,40)
+	bob4 = new bob(440,500,40)
+	bob5 = new bob(480,500,40)
 	
 	rope1=new rope(bob1.body,roofObject.body,-80)
 	rope2=new rope(bob2.body,roofObject.body,-40)
@@ -47,6 +47,10 @@ function setup() {
 	rope4=new rope(bob4.body,roofObject.body,40)
 	rope5=new rope(bob5.body,roofObject.body,80)
 	
+	side=new rooves(200,420,350,20);
+	side1=new rooves(600,420,350,20);
+	side2=new rooves(400,590,20,500);
+
 	Engine.run(engine);
 	
 }
@@ -55,6 +59,10 @@ function draw() {
   rectMode(CENTER);
   background("skyBlue");
   roofObject.display();
+
+  side.display();
+  side1.display();
+  side2.display();
 
   rope1.display();
   rope2.display();
@@ -69,45 +77,36 @@ function draw() {
   bob4.display();
   bob5.display();
 
-  keyPressed();
-
-	N = createSprite(100,30);
-	E = createSprite(200,30);
-	W = createSprite(300,30);
-	T = createSprite(400,30);
-	O = createSprite(500,30);
-	Ns = createSprite(600,30);
-	S = createSprite(700,70);
-	C = createSprite(40,70);
-	R = createSprite(190,70);
-	A = createSprite(350,70);
-	D = createSprite(500,70);
-	L = createSprite(650,70);
-	Es = createSprite(700,70);
-
-	N.addImage(NImg);
-	E.addImage(EImg);
-	W.addImage(WImg);
-	T.addImage(TImg);
-	O.addImage(OImg);
-	Ns.addImage(NImg);
-	S.addImage(SImg);
-	C.addImage(CImg);
-	R.addImage(RImg);
-	A.addImage(AImg);
-	D.addImage(DImg);
-	L.addImage(LImg);
-	Es.addImage(EImg);
+	image(NImg,100,30,60,90);
+	image(EImg,200,30,60,90);
+	image(WImg,300,30,60,90);
+	image(TImg,400,30,60,90);
+	image(OImg,500,30,60,90);
+	image(NImg,600,30,60,90);
+	image(SImg,700,30,60,90);
+	image(CImg,40,110,60,90);
+	image(RImg,180,110,60,90);
+	image(AImg,320,110,60,90);
+	image(DImg,460,110,60,90);
+	image(LImg,600,110,60,90);
+	image(EImg,740,110,60,90);
 
 
 	drawSprites();
+
+	strokeWeight(4);
+	fill("pink");
+	stroke("black");
+	textSize(20);
+	textFont("Times New Roman");
+	text("Press 'up' to see the magic!",300, 240);
 }
 
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
 //WRITE A CORRECT CODE TO APPLY A KEYPRESSED TO CHANGE THE POSITION OF BALL OBJECT TO THE LEFT WHEN UP ARROW KEY IS PRESSED
-Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-40});
+Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-15,y:-40});
 	}
 }
 
